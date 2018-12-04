21101158f107a0c3427eb5f9ee8313fb86af7737
DJRockyFresh <43992131+DJRockyFresh@users.noreply.github.com>
2018-12-04 14:56:04 +0000

    Update isd0904.py

--- isd0904.py
+++ isd0904.py
@@ -1,4 +1,10 @@
+page = input("Enter something: ")
+print(page)
+pagenum = 0
+
+
 if page % 2 == 0:
     print(page)
 else:
     print("%60s%d" % (" ", page))
+# input something = page, page number should be in the next row.
